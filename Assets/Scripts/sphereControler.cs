e17ea31bc08de11d09354c3eb6cafa7575dce373
altGreG <grzeg.bak.pl@gmail.com>
2023-10-14 13:20:37 +0200

    dodanie dwóch kolejnych kierunków ruchu piłki

--- Assets/Scripts/sphereControler.cs
+++ Assets/Scripts/sphereControler.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class sphereControler : MonoBehaviour // Dziedziczenie po klasie MonoBehaviour, tam są metody które używamy np. transform
 {
-
-    public float speed = 2f; // zmienne publiczne można edytować z poziomu edytora unity
+    // [HideInInspector]
+    // public float speed = 2f; // zmienne publiczne można edytować z poziomu edytora unity
+    
+    [Header("Control Settings")]
+    [SerializeField]
+    private float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +22,32 @@ public class sphereControler : MonoBehaviour // Dziedziczenie po klasie MonoBeha
     // Update is called once per frame
     void Update()
     {
+        float direction;
+        Debug.Log(Input.GetAxis("Horizontal"));
+
+        if((direction = Input.GetAxis("Horizontal")) != 0) // pobiera oś z ustaloną w input managerze
+        { 
+            Debug.Log("Klikamy strzałki!");
+        } 
+
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            //transform.position = new Vector3(transform.position.x-1f * Time.deltaTime * speed, transform.position.y, transform.position.z); - słabe rozw. 
             transform.Translate(-1f * Time.deltaTime * speed,0, 0, Space.World); // Space.World po zrotowaniu dalej będzie szła kula w lewo
         }
         
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(1f * Time.deltaTime * speed,0, 0, Space.World); // translacja nie niweluje pędu, to jak teleportacja xD
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0,0, 1f * Time.deltaTime * speed, Space.World); 
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,0, -1f * Time.deltaTime * speed, Space.World); 
         }
     }
 }
